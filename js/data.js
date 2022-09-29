0068f2c598bd91ae884c9e0a892cf86acbfa4a0a
Richard <richard.balakrishnan@syncfusion.com>
2022-09-29 17:12:10 +0530

    init

--- js/data.js
+++ js/data.js
@@ -1,107 +1,170 @@
 const reportData = [
   {
-    name: "Sales Analysis",
-    title: "Sales Analysis",
-    description: "Sales Analysis",
-    Image: 'solution',
-    category: 'Sales'
+    name: "Barcode types",
+    title: "Barcode types",
+    description: "The barcode demo shows the available 1D, 2D and QR barcode types rendered through RDL Custom Report Item.",
+    Image: "barcode types",
+    category: "Barcode"
   },
   {
-    name: "Northwind",
-    title: "Northwind",
-    description: "Northwind",
-    Image: 'server',
-    category: 'Sales'
+    name: "Product details",
+    title: "Product details",
+    description: "The product details using the SSRS table report item with barcodes rendered through RDL Custom Report Item.",
+    Image: "product details",
+    category: "Barcode"
   },
   {
-    name: "Financial Analysis",
-    title: "Financial Analysis",
-    description: "Financial Analysis",
-    Image: 'solution',
-    category: 'Sales'
+    name: "Company sales",
+    title: "Company sales",
+    description: "Company Sales report shows product sales against year in matrix format using SSRS Tablix report item.",
+    Image: "Company sales",
+    category: "Sales"
   },
   {
-    name: "Student Details",
-    title: "Student Details",
-    description: "Student Details",
-    Image: 'server',
-    category: 'Education'
+    name: "Northwind products and suppliers",
+    title: "Northwind products and suppliers",
+    description: "This demo shows the product and suppliers details using SSRS pyramid, bar, column charts and table widget.",
+    Image: "Northwind products",
+    category: "Visualization"
   },
   {
-    name: "Higher Education",
-    title: "Higher Education",
-    description: "Higher Education",
-    Image: 'solution',
-    category: 'Education'
+    name: "Data bar",
+    title: "Data bar",
+    description: "This represents company sales against year in table format with data bar report item.",
+    Image: "data bar",
+    category: "Visualization"
   },
   {
-    name: "School Performance",
-    title: "School Performance",
-    description: "School Performance",
-    Image: 'server',
-    category: 'Education'
+    name: "Dynamic chart series",
+    title: "Dynamic chart series",
+    description: "This allows you to choose the chart series dynamically.",
+    Image: "dynamic chart series",
+    category: "Visualization"
   },
   {
-    name: "Ads Campaign Performance",
-    title: "Ads Campaign Performance",
-    description: "Ads Campaign Performance",
-    Image: 'server',
-    category: 'Marketing'
+    name: "Product line sales",
+    title: "Product line sales",
+    description: "The Product Line Sales report displays sales details organized by category and subcategory of products using cascading parameters.",
+    Image: "product line sales",
+    category: "Sales"
   },
   {
-    name: "Online Marketing",
-    title: "Online Marketing",
-    description: "Online Marketing",
-    Image: 'solution',
-    category: 'Marketing'
+    name: "Website visitor analysis",
+    title: "Website visitor analysis",
+    description: "This RDL report template shows website visitor analysis details using SSRS column chart, pie chart, line chart and table data item.",
+    Image: "Website Visitor Analysis",
+    category: "Analysis"
   },
   {
-    name: "Website Analytics",
-    title: "Website Analytics",
-    description: "Website Analytics",
-    Image: 'server',
-    category: 'Marketing'
+    name: "Ticket sales analysis",
+    title: "Ticket sales analysis",
+    description: "This RDL report template shows website visitor analysis details using SSRS column chart, pie chart, line chart and table data item.",
+    Image: "Ticket Sales Analysis",
+    category: "Analysis"
   },
   {
-    name: "General Management",
-    title: "General Management",
-    description: "General Management",
-    Image: 'server',
-    category: 'Telecommunication'
+    name: "Consolidated balance report",
+    title: "Consolidated balance report",
+    description: "The consolidated balance sheet RDLC report represents the assets and liabilities of a company.",
+    Image: "consolidated balance report",
+    category: "Sales"
   },
   {
-    name: "Customer Service Overview",
-    title: "Customer Service Overview",
-    description: "Customer Service Overview",
-    Image: 'solution',
-    category: 'Telecommunication'
+    name: "Customer details",
+    title: "Customer details",
+    description: "The customer details in SSRS table report item and details are highlighted based on conditional row formatting.",
+    Image: "customer details",
+    category: "Analysis"
   },
   {
-    name: "Network Performance",
-    title: "Network Performance",
-    description: "Network Performance",
-    Image: 'server',
-    category: 'Telecommunication'
+    name: "Customer support analysis",
+    title: "Customer support analysis",
+    description: "Shows customer support analysis with SSRS drill through to explore details in another report based on user click action.",
+    Image: "customer support analysis",
+    category: "Analysis"
   },
   {
-    name: "Retail Management",
-    title: "Retail Management",
-    description: "Retail Management",
-    Image: 'server',
-    category: 'Retail'
+    name: "Dynamic columns",
+    title: "Dynamic columns",
+    description: "This allows you to choose the column of the table dynamically.",
+    Image: "dynamic columns",
+    category: "Analysis"
   },
   {
-    name: "Retail Service Overview",
-    title: "Retail Service Overview",
-    description: "Retail Service Overview",
-    Image: 'solution',
-    category: 'Retail'
+    name: "Invoice",
+    title: "Invoice",
+    description: "This demo shows the sales invoice details and the usage of report viewer parameters, expressions, and calculated fields using SSRS Tablix report item.",
+    Image: "Invoice",
+    category: "Sales"
   },
   {
-    name: "Network Performance",
-    title: "Network Performance",
-    description: "Network Performance",
-    Image: 'server',
-    category: 'Retail'
+    name: "Mail merge",
+    title: "Mail merge",
+    description: "Mail merge shows to envelope the employee details using SSRS List report item.",
+    Image: "Mail Merge",
+    category: "Analysis"
+  },
+  {
+    name: "Patient experience analysis",
+    title: "Patient experience analysis",
+    description: "This demo shows the patient analysis report of a health care using SSRS doughnut chart, line chart and basic table data regions.",
+    Image: "patient experience analysis",
+    category: "Analysis"
+  },
+  {
+    name: "Paystub",
+    title: "Paystub",
+    description: "This demos shows the Paystub report represents the payslip of a company.",
+    Image: "Paystub",
+    category: "Analysis"
+  },
+  {
+    name: "Personal expense analysis",
+    title: "Personal expense analysis",
+    description: "This report shows personal expense analysis details using SSRS image, text box, line, rectangle, chart and table report items.",
+    Image: "personal expense analysis",
+    category: "Analysis"
+  },
+  {
+    name: "Product catalog",
+    title: "Product catalog",
+    description: "This demos allows you to view the product catalog RDLC that exist on the local file system with JSON array and custom business object data collection.",
+    Image: "product catalog",
+    category: "Analysis"
+  },
+  {
+    name: "Product sales",
+    title: "Product sales",
+    description: "This demo shows product sales total based on quarterly wise RDL tablix grouping.",
+    Image: "product sales",
+    category: "Sales"
+  },
+  {
+    name: "Sales by year",
+    title: "Sales by year",
+    description: "This demos allows you to view the sales by year RDLC that exist on the local file system with JSON array and custom business object data collection.",
+    Image: "sales by year",
+    category: "Sales"
+  },
+  {
+    name: "Sales order details",
+    title: "Sales order details",
+    description: "This demo shows the sales order details using RDL basic table with currency formatting in price textbox fields.",
+    Image: "sales order details",
+    category: "Sales"
+  },
+  {
+    name: "Sales order",
+    title:"Sales order",
+    description: "This report template shows the sales order details based on order number using the RDL report parameters, rectangle, line and table.",
+    Image: "sales order",
+    category: "Sales"
+  },
+  {
+    name: "Territory sales",
+    title:"Territory sales",
+    description: "This demo shows the sales territory and its total sales using drilldown with SSRS matrix report.",
+    Image: "Territory Sales",
+    category: "Sales"
   }
 ];
